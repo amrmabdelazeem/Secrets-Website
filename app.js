13164f06146d8a4b78283f690dc855d6afafb672
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-04-15 15:48:00 +0200

    Level one security save username and password

---
+++ app.js
@@ -0,0 +1,68 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/",(req, res)=>{
+    res.render("home");
+})
+app.get("/login",(req, res)=>{
+    res.render("login");
+})
+app.get("/register",(req, res)=>{
+    res.render("register");
+})
+
+app.listen(3000, ()=>{
+    console.log("Server started on port 3000");
+})
+
+app.post("/register", (req, res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    try {
+        newUser.save();
+        console.log("Successfully saved new user!");
+        res.render("secrets");
+    } catch (err) {
+        res.send(err);
+    }
+
+});
+
+app.post("/login", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}).then((foundUser)=>{
+        if(foundUser){
+            if(foundUser.password === password){
+                res.render("secrets");
+            }else{
+                res.send("Password inccorect");
+            }
+        }
+    }).catch(err=>{
+        res.send(err);
+    })
+})
