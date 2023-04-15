76775f05304c81ad7e30852501f057477227d127
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-04-15 16:05:14 +0200

    Level 2 security with encrypt

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisoutlittlesecret.";
+userSchema.plugin(encrypt, {secret:secret, encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
